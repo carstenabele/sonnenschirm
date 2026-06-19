b1a5bf60f5c665428978d4d7d3f1a59fd1d06049
Carsten Abele <carsten@ravensburg.dev>
2026-06-19 18:36:22 +0200

    feat(ios): XcodeGen-Gerüst, App baut im Simulator

---
+++ ios/Sources/ContentView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color.black.ignoresSafeArea()
+            Text("Schattenwerfer")
+                .foregroundStyle(.white)
+                .font(.title2)
+        }
+    }
+}
